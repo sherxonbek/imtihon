01e80d64aa78c6b51c0df5343a3fdf279a547b33
fozilgulomiddinov-ctrl <fozilgulomiddinov>
2026-07-11 16:54:44 +0500

    iconlarni qoshdim

--- src/admin/pages/Dashboard.jsx
+++ src/admin/pages/Dashboard.jsx
@@ -24,10 +24,27 @@ function Dashboard() {
       <h1>Dashboard</h1>
       <span>Bugungi ko'rsatkichlar - {today}</span>
       <div className="flex gap-2 mt-">
-        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
-        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
-        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
-        
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl">
+          <i className="bx bx-line-chart text-blue-500 text-3xl m-2"></i>
+          <span className="text-[#6B7280] flex text-[11.3px] mx-2">
+            Umumiy sotuv
+          </span>
+          <h2></h2>
+        </div>
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl">
+          <i className="bx bx-shopping-bag text-blue-500 text-3xl m-2"></i>
+          <span className="text-[#6B7280] flex text-[11.3px] mx-2">
+            Umumiy sotuv
+          </span>
+          <h2></h2>
+        </div>
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl">
+          <i className="bx bx-group text-blue-500 text-3xl m-2"></i>
+          <span className="text-[#6B7280] flex text-[11.3px] mx-2">
+            Umumiy sotuv
+          </span>
+          <h2></h2>
+        </div>
       </div>
     </div>
   );

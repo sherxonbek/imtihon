485ec25433423bf0a0d7f3b49b66e14042852d63
fozilgulomiddinov-ctrl <fozilgulomiddinov>
2026-07-11 16:42:31 +0500

    kodni dabdala qildim

--- src/admin/pages/Dashboard.jsx
+++ src/admin/pages/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+// import 'boxicons/css/boxicons.min.css';
+const months = [
+  "yanvar",
+  "fevral",
+  "mart",
+  "aprel",
+  "may",
+  "iyun",
+  "iyul",
+  "avgust",
+  "sentabr",
+  "oktabr",
+  "noyabr",
+  "dekabr",
+];
+
+const date = new Date();
+const today = `${date.getDate()}-${months[date.getMonth()]}, ${date.getFullYear()}`;
 
 function Dashboard() {
   return (
-    <div>Ishlamaydigan xona xozircha bosh</div>
-  )
+    <div>
+      <h1>Dashboard</h1>
+      <span>Bugungi ko'rsatkichlar - {today}</span>
+      <div className="flex gap-2 mt-">
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
+        <div className="w-1/3 h-[112.6px] bg-gray-200 rounded-2xl"></div>
+        
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
